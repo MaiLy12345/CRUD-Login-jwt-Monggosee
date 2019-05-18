999899a72da04d1ab1366c93e9d114319ae7b312
Mai Ly <taikhoan835@gmail.com>
2019-05-19 01:03:11 +0700

    update sencondly

--- apis/user.js
+++ apis/user.js
@@ -7,8 +7,8 @@ const Joi =  require('joi');
 
 
 exports.load = (app) => {
-    app.post('/api/v1/users', validate(userValidation.creatUser()), controllerUser.createUser);
     app.post('/api/v1/login', controllerUser.login);
+    app.post('/api/v1/users', validate(userValidation.creatUser()), controllerUser.createUser);
     app.delete('/api/v1/users/:id', validate(userValidation.deleteUser()) , controllerUser.deleteUser);
     app.get('/api/v1/users/:id', authentication, validate(userValidation.getUser()) , controllerUser.getUser);
     app.get( '/api/v1/users', controllerUser.getListUser);
